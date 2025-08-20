8d6f9ac15c9af9bf7954432eb8109eb43bd2e18e
Floriana08 <fl.dibuono@hotmail.it>
2025-08-20 14:45:11 +0100

    added API

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = response.data.temperature.current;
+  console.log(response.data.temperature.current);
+}
+
+function searchCity(city) {
+  let apiKey = "af99134e8ac39c5b63393a3f6a0o43tb";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function ShowCity(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#weather-app-city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
